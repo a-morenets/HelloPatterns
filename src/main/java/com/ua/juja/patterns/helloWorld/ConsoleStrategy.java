3038df4be8ee43c2f57cd499a8aef41ccf640d51
Oleksii Morenets <alexey.morenets@gmail.com>
2018-01-12 23:01:28 +0200

    Ура, у нас есть Singleton!

--- src/main/java/com/ua/juja/patterns/helloWorld/ConsoleStrategy.java
+++ src/main/java/com/ua/juja/patterns/helloWorld/ConsoleStrategy.java
@@ -1,6 +1,16 @@
 package com.ua.juja.patterns.helloWorld;
 
-public class ConsoleStrategy implements Strategy {
+public final class ConsoleStrategy implements Strategy {
+    private static Strategy instance;
+
+    private ConsoleStrategy() {
+        // do nothing
+    }
+
+    public static Strategy getInstance() {
+        return instance = instance == null ? new ConsoleStrategy() : instance;
+    }
+
     public void print(String message) {
         System.out.println(message);
     }
--- src/main/java/com/ua/juja/patterns/helloWorld/Main.java
+++ src/main/java/com/ua/juja/patterns/helloWorld/Main.java
@@ -2,9 +2,7 @@ package com.ua.juja.patterns.helloWorld;
 
 public class Main {
     public static void main(String[] args) {
-        Strategy strategy = new ConsoleStrategy();
-
-        new Runner(strategy).run();
+        new Runner(ConsoleStrategy.getInstance()).run();
     }
 
 }
